c3d9ddb6a3d2ba33212155c094d21df2b7d90ff2
Brian Heim <bheim67@gmail.com>
2016-11-02 09:48:44 -0600

    now have collection of BestBuyStores PORO, working on methods to display

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,15 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get 'https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone,region&pageSize=15&apiKey=nysued5unnmfhft4bp5u6779'
+    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{params["q"]},25))?format=json&show=storeId,storeType,longName,city,distance,phone,region&pageSize=15&apiKey=nysued5unnmfhft4bp5u6779"
     @stores = JSON.parse(response.body, symbolize_names: true)
+
+    @total_count = @stores[:total]
+
+    @stores_array = @stores[:stores]
+
+    @found_stores = @stores_array.map do |raw_store|
+      BestBuyStore.new(raw_store)
+    end
     binding.pry
   end
 end
---
+++ app/models/best_buy_store.rb
@@ -0,0 +1,12 @@
+class BestBuyStore
+  def initialize(attrs)
+    @attrs = attrs
+  end
+
+  def name
+    
+  end
+
+  private
+    attr_reader :attrs
+end
