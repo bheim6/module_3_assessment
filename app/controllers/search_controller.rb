9c5f4ccbe58a21afd6c76ccb27e5163cc32a9a14
Brian Heim <bheim67@gmail.com>
2016-11-02 09:34:28 -0600

    got api call to work, now working through parsing and representing on index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-
+    response = Faraday.get 'https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone,region&pageSize=15&apiKey=nysued5unnmfhft4bp5u6779'
+    @stores = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
   end
 end
--- spec/features/user_search_stores_spec.rb
+++ spec/features/user_search_stores_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "User can search for stores" do
     # When I visit "/"
     visit '/'
     # And I fill in a search box with "80202" and click "search"
-    fill_in "Zip", with: "80202"
+    fill_in "Search for:", with: "80202"
     click_on "Search"
     # Then my current path should be "/search" (ignoring params)
     expect(current_path).to eq('/search')
@@ -15,7 +15,7 @@ RSpec.feature "User can search for stores" do
     expect(page).to have_content("17 Total Stores")
     # And I should see exactly 15 results
     # within(".stores") do
-    #   
+    #
     # end
     # And I should see the long name, city, distance, phone number and store type for each of the 15 results
   end
